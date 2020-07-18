0b077eb56cb7f552b9a6708f1418a959f7d292e7
Pierre <pi.laloy@laposte.net>
2020-07-18 22:53:19 +0200

    Jour 6 : Fin de la leçon Ownership/Borrowing

--- borrowingPrincipe/src/main.rs
+++ borrowingPrincipe/src/main.rs
@@ -1,15 +1,28 @@
 fn main() {
-let s = String::from("String");
-let y = &s;
-println!("{}", s);
+    
+    // exemple simplifié de Ownership/borrowing : 
+    let s = String::from("String");
+    let y = &s;
+    println!("{}", s);
 
-
-//_________________
-exemple_moving();
-exemple_copying();
+    println!("________MOVING________");
+    exemple_moving();
+    println!("_______________________");
+    println!("________COPYING________");
+    exemple_copying();
+    println!("_________________________");
+    println!("________BORROWING________");
+    exemple_borrowing();
 }
 
-//exemple de moving on prend le controle et on ne le rend jamais. la variable est detruite hors contexte ensuite.
+/*  ____________________
+    |exemple de moving.|
+    --------------------
+    moving : on prend la proprietes (Ownership) de la variable et on ne le rend jamais.
+    la variable est detruite hors contexte ensuite.
+
+    remarque : il a lieu avec des donnees composees.
+*/
 fn exemple_moving(){
 
     let mut vec = Vec ::new();
@@ -25,13 +38,24 @@ fn exemple_moving(){
 
 }
 
+/*
+    lors d'un mov c'est la fonction qui reccupere la propriete de la donnee elle ne la rend jamais
+*/
 fn moving (v:Vec<i32>){
     println!("addition de valeur de v : {}", v[10] + v[70]);
 }
 
-//_______________________________
 
-//exemple de copying les varaible a et b existe dans les deux scope en meme temps, pas genant nous sommes dans la stack avec dce type de donées scalaire.
+
+/*  
+    _____________________
+    |exemple de copying |
+    ---------------------
+    copying : les varaibles a et b existe dans les deux scopes en meme temps, c'est stocke dans la stack. 
+    
+    remarque : il a lieu lorsque l'on est avec des donnee scalaire.
+
+*/
 fn exemple_copying(){
 
    let a = 32;
@@ -40,7 +64,7 @@ fn exemple_copying(){
     cop(a,b);
   
     
-    println!("on a utiliser les varaible a : {} et b : {} ",a ,b );
+    println!("on a utiliser les variables a : {} et b : {} ",a ,b );
 }
 
 fn cop (a:i32, b:i32){
@@ -48,6 +72,44 @@ fn cop (a:i32, b:i32){
     println!("addtion params a et b : {}", a + b);
 }
 
-//_______________________________
+/*  
+    _______________________
+    |exemple de borrowing |
+    -----------------------
+    Avoir plusieurs réferences sur la même valeur. Les références sont aussi des objets. Les références ressemble aux pointeurs. 
+    Les reference mutables sont move les reference imutable sont copy. Quand une refrence est hors scope drop le borrowing s'arette
+*/
+fn re (v: Vec<i32>)->Vec<i32>{
+    println!("{}",v[120] + v[111]);
+    v
+}
 
-//exemple de copying
+fn borrow_1(v: &Vec<i32>){
+    println!("{}",(*v)[10] + (*v)[12]);
+}
+fn borrow_2(v: &Vec<i32>){
+    println!("{}",v[10] + v[11]);
+}
+
+
+
+fn exemple_borrowing(){
+    let mut v = Vec::new();
+
+
+    for i in 1..1000{
+        v.push(i)
+    }
+
+    v = re(v);
+    println!("Possède toujours v: {} {}", v[0], v[1]);
+    
+    borrow_1(&v);
+    println!("Possède toujours v: {} {}", v[0], v[1]);
+    
+    borrow_1(&v);
+    println!("Possède toujours v: {} {}", v[0], v[1]);
+}
+
+
+
